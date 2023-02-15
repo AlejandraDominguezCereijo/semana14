74e38716cea02147b88efcaec3afbe341b0a740a
AlejandraDominguezCereijo <adominguezcereijo@danielcastelao.org>
2023-02-15 17:53:58 +0100

    Correccion trigrono

--- src/Trigono.java
+++ src/Trigono.java
@@ -1,24 +1,18 @@
-public class Trigono extends Cuadrilatero implements Forma {
-    public Trigono() {
-    }
-    @Override
-    public void dibujar_puntos() {
+public abstract class Trigono {
+    private Punto[] vertices = new Punto[3];
 
-    }
-
-    @Override
-    public void rellenar_forma() {
-
+    public Punto[] getVertices() {
+        return this.vertices;
     }
 
-    @Override
-    public double calcular_area() {
-        return 0;
+    public void setPuntos(Punto[] vertices) {
+        this.vertices = vertices;
     }
 
-    @Override
-    public double calcular_perimetro() {
-        return 0;
+    public void setVertices(Punto p1, Punto p2, Punto p3) {
+        vertices[0] = p1;
+        vertices[1] = p2;
+        vertices[2] = p3;
     }
 
 
