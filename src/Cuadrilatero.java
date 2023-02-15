612c39b947d3f758faa9365d08a3e2b60d977e89
AlejandraDominguezCereijo <adominguezcereijo@danielcastelao.org>
2023-02-15 18:10:24 +0100

    Correccion cuadrilatero

--- src/Cuadrilatero.java
+++ src/Cuadrilatero.java
@@ -1,18 +1,20 @@
 public abstract class Cuadrilatero {
-    private Punto [] vertices=new Punto[4];
+    private Punto[] vertices = new Punto[4];
 
-    public Punto[] getVertices(){
+    public Punto[] getVertices() {
         return this.vertices;
     }
-    public void setPuntos(Punto [] vertices){
-        this.vertices=vertices;
-    }
-    public void setVertices(Punto p1,Punto p2,Punto p3,Punto p4){
-        vertices[0]=p1;
-        vertices[1]=p2;
-        vertices[2]=p3;
-        vertices[3]=p4;
+
+    public void setVertices(Punto[] vertices) {
+        this.vertices = vertices;
     }
 
-//aqui falta un metodo abstracto para que la clase sea abstracta de verdad, sin metodo abstracto no tenemos clase abstracta
+    public void setVertices(Punto p1, Punto p2, Punto p3, Punto p4) {
+        vertices[0] = p1;
+        vertices[1] = p2;
+        vertices[2] = p3;
+        vertices[3] = p4;
+    }
+
+
 }
