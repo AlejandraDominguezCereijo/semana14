ff31150e9166256ccb19a9e63a85cde8c7b44834
AlejandraDominguezCereijo <adominguezcereijo@danielcastelao.org>
2023-02-16 11:50:50 +0100

    Añadir bucle for each

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         Cuadrado _cuadrado = new Cuadrado(5);
-        /*_cuadrado.dibujar_puntos();
-        _cuadrado.rellenar_forma();
-        System.out.println("El area del cuadrado es: "+_cuadrado.calcular_area());
-        System.out.println("El perimetro del cuadrado es: "+_cuadrado.calcular_perimetro());*/
-        Rectangulo _rectangulo = new Rectangulo(8,5);
-       /* _rectangulo.dibujar_puntos();
-        _rectangulo.rellenar_forma();
-        System.out.println("El area del rectangulo es: "+_rectangulo.calcular_area());
-        System.out.println("El perimetro del rectangulo es: "+_rectangulo.calcular_perimetro());*/
-        Triangulo _triangulo = new Triangulo(5,8);
-        /*_triangulo.dibujar_puntos();
-        _triangulo.rellenar_forma();
-        System.out.println("El area del triangulo es: "+_triangulo.calcular_area());
-        System.out.println("El perimetro del triangulo es: "+_triangulo.calcular_perimetro());*/
+     
+        Rectangulo _rectangulo = new Rectangulo(8, 5);
+
+        Triangulo _triangulo = new Triangulo(5, 8);
+
         Forma[] array_forma = new Forma[3];
         array_forma[0] = _cuadrado;
         array_forma[1] = _rectangulo;
         array_forma[2] = _triangulo;
-        for (Forma f:array_forma) {
+        for (Forma f : array_forma) {
             f.dibujar_puntos();
             f.rellenar_forma();
             f.calcular_area();
